b3571618af524559db414f5c6b655a31fadbb662
preethamkt46 <preethamkt18@gmail.com>
2025-01-29 16:29:48 +0530

    Initial commit

--- app/src/main/java/np/com/bimalkafle/easybot/Constants.kt
+++
@@ -1,5 +0,0 @@
-package np.com.bimalkafle.easybot
-
-object Constants {
-    val apiKey = "YOUR-API-KEY"
-}
--- app/src/main/java/np/com/bimalkafle/easybot/MessageModel.kt
+++
@@ -1,6 +0,0 @@
-package np.com.bimalkafle.easybot
-
-data class MessageModel(
-    val message : String,
-    val role : String,
-)
